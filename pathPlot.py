4f5e8abc6593e579962090daf787a2e393783464
Lucas Cavalcanti <lhcs@cin.ufpe.br>
2021-05-25 21:28:58 -0300

    ✨ Ploting new data

--- pathPlot.py
+++ pathPlot.py
@@ -4,16 +4,12 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
-odmFile = 'data/robotOdm_clear.csv'
-visFile = 'data/robotVis_clear.csv'
+robotFile = 'data/test_1_05-25/robotInfo_clear.csv'
 
-odmX = []
-odmY = []
-odmW = []
 odm = []
 vis = []
 
-with open(odmFile) as csv_file:
+with open(robotFile) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
@@ -21,41 +17,20 @@ with open(odmFile) as csv_file:
             print("Column names are ", ", ".join(row))
             line_count += 1
         else:
-            odmX.append(float(row[1]))
-            odmY.append(float(row[2]))
-            w = float(row[3])
-            # if w < 0:
-            #     w = w+2*math.pi
-            odmW.append(w)
-            odm.append([float(row[1]), float(row[2]), w])
+            odm.append([float(row[1]), float(row[2]), float(row[3])])
+            vis.append([float(row[4]), float(row[5]), float(row[6])])
             line_count += 1
     print('Processed {0} lines.'.format(line_count))
-
-with open(visFile) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print("Column names are ", ", ".join(row))
-            line_count += 1
-        else:
-            w = float(row[3])
-            # if w < 0:
-            #     w = w+2*math.pi
-            vis.append([float(row[1]), float(row[2]), w])
-            line_count += 1
-    print('Processed {0} lines.'.format(line_count))
-
 
 odometry = np.array(odm)
-odmOrigin = odometry[:, 0:2]
+odmOrigin = odometry[:, 0:3]
 # print(odmOrigin.shape)
 odmVector = np.array([odometry[:, 0] + np.cos(odometry[:, 2]),
                       odometry[:, 1] + np.sin(odometry[:, 2])]).T
 # print(odmVector.shape)
 
 vision = np.array(vis)
-visOrigin = vision[:, 0:2]
+visOrigin = vision[:, 0:3]
 visVector = np.array([vision[:, 0] + np.cos(vision[:, 2]),
                       vision[:, 1] + np.sin(vision[:, 2])]).T
 
@@ -63,25 +38,31 @@ visVector = np.array([vision[:, 0] + np.cos(vision[:, 2]),
 # plt.quiver(odmOrigin[:, 0], odmOrigin[:, 1], odmVector[:, 0], odmVector[:, 1])
 
 
-fig, (ax1, ax2) = plt.subplots(2)
+fig, (visPlot, odmPlot) = plt.subplots(2)
 
-ax1.plot(visOrigin[:, 0], visOrigin[:, 1])
+visPlot.plot(visOrigin[:, 0], visOrigin[:, 1], 'r')
 
-ax1.set(xlabel='x (m)', ylabel='y (m)',
+visPlot.set(xlabel='x (m)', ylabel='y (m)',
        title='Vision output points')
 
-ax2.plot(odmOrigin[:, 0], odmOrigin[:, 1])
+odmPlot.plot(odmOrigin[:, 0], odmOrigin[:, 1], 'g')
 
-ax2.set(xlabel='x (m)', ylabel='y (m)',
+odmPlot.set(xlabel='x (m)', ylabel='y (m)',
         title='Odometry output points')
 
-fig2, ax = plt.subplots()
+fig2, (bothPlot, bothW) = plt.subplots(2)
 
-ax.plot(visOrigin[:, 0], visOrigin[:, 1], 'r')
-ax.plot(odmOrigin[:, 0], odmOrigin[:, 1], 'g')
+bothPlot.plot(visOrigin[:, 0], visOrigin[:, 1], 'r')
+bothPlot.plot(odmOrigin[:, 0], odmOrigin[:, 1], 'g')
 
-ax.set(xlabel='x (m)', ylabel='y (m)',
+bothPlot.set(xlabel='x (m)', ylabel='y (m)',
         title='Vision and Odometry output points')
+
+bothW.plot(range(len(visOrigin[:, 2])), visOrigin[:, 2], 'r')
+bothW.plot(range(len(odmOrigin[:, 2])), odmOrigin[:, 2], 'g')
+
+bothW.set(xlabel='x (m)', ylabel='y (m)',
+        title='Vision and Odometry output angles')
 
 
 # fig.savefig("test.png")
